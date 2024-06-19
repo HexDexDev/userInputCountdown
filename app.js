a54e8f101f3f095a60a24d44922caacab8e11241
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 19:52:30 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// Pede um número ao usuário
+let numeroDoUsuario = prompt("Digite um número");
+
+// Cria uma regra que se o número for maior que 0
+while (numeroDoUsuario >= 0) {
+// emite um alerta com o número do usuário
+alert (numeroDoUsuario);
+// e vai reduzindo até chegar no número definido na linha 5
+numeroDoUsuario--
+}
