3bf0e645530d351ec54ee392fc45f71cd73dd9b3
HexDexDev <138143730+HexDexDev@users.noreply.github.com>
2024-06-19 22:24:42 -0300

    replace alert with console.log to fix the code

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let numeroDoUsuario = prompt("Digite um número");
 // Cria uma regra que se o número for maior que 0
 while (numeroDoUsuario >= 0) {
 // emite um alerta com o número do usuário
-alert (numeroDoUsuario);
+    console.log (numeroDoUsuario);
 // e vai reduzindo até chegar no número definido na linha 5
-numeroDoUsuario--
+    numeroDoUsuario--;
 }
